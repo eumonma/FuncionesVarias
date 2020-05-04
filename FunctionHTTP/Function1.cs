f486a3d2416bf81fae5756e1dc7beda1ffb6eacb
Eugenio Montoro Mateos <eugenio.montoro@gmail.com>
2020-05-04 22:18:18 +0200

    Incorporación de los ficheros "local.settings.json"

--- FunctionHTTP/Function1.cs
+++ FunctionHTTP/Function1.cs
@@ -19,6 +19,7 @@ namespace FunctionHTTP
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
+
             string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
