a8594e3e01d618aef527dcfe6e71a395654b6611
Eugenio Montoro Mateos <eugenio.montoro@gmail.com>
2020-05-07 22:20:35 +0200

    Insertat en Table Storage local

--- FunctionHTTP/InsertPersona.cs
+++ FunctionHTTP/InsertPersona.cs
@@ -7,15 +7,56 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Microsoft.Azure.WebJobs.Extensions.Storage;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
 
 namespace FunctionHTTP
 {
     public static class InsertPersona
     {
+        private const string TableName = "MyTable";
+
+        
+        public class Todo
+        {
+            public string Id { get; set; } = Guid.NewGuid().ToString("n");
+            public DateTime CreatedTime { get; set; } = DateTime.UtcNow;
+            public string Nombre { get; set; }
+            public bool IsCompleted { get; set; }
+        }
+
+        public static TodoTableEntity ToTableEntity(this Todo todo)
+        {
+            return new TodoTableEntity()
+            {
+                PartitionKey = "TODO",
+                RowKey = todo.Id,
+                CreatedTime = todo.CreatedTime,
+                IsCompleted = todo.IsCompleted,
+                NombreEmpleado = todo.Nombre
+            };
+        }
+
+        public class TodoTableEntity : TableEntity
+        {
+            public DateTime CreatedTime { get; set; }
+            public string NombreEmpleado { get; set; }
+            public bool IsCompleted { get; set; }
+        }
+
+        public class TodoCreateModel
+        {
+            public string Name { get; set; }
+        }
+
         [FunctionName("InsertPersona")]
+//        [return: Table("MyTable")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+            [Table(TableName, Connection = "AzureWebJobsStorage")] IAsyncCollector<TodoTableEntity> todoTable,
+             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
@@ -23,18 +64,11 @@ namespace FunctionHTTP
             string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var data = JsonConvert.DeserializeObject<TodoCreateModel>(requestBody);
 
-//            log.LogInformation(requestBody);
-//          Comentario 2
-
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            var todo = new Todo() { Nombre = data.Name };
+            await todoTable.AddAsync(todo.ToTableEntity());
+            return new OkObjectResult(todo);
         }
     }
 }
